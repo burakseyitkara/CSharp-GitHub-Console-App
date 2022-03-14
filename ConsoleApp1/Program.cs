0a10055ef9023e5d06c7e887917ddf992c99a3aa
Burak Seyit Kara <burakseyitkara@icloud.com>
2022-03-14 11:01:44 +0300

    b'ler eklendi

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,3 +9,9 @@ static void Main(string[] args)
     Console.WriteLine("yedek brranchi knk");
     Console.WriteLine("yedek brranchi knk");
 }
+
+
+Console.WriteLine("b1");
+Console.WriteLine("b2");
+Console.WriteLine("b3");
+Console.WriteLine("b4");
